6e5365d20388aa864a3111c287d385deaf0058be
swittv <4galarzajuan2004@gmail.com>
2022-05-30 10:43:50 -0300

    configured environment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
 const url = `https://api.themoviedb.org/3/movie/2?api_key=cb38c927d80947fc044c7b82bf521426`
+
+console.log(api_key)
---
+++ js/secrets.example.js
@@ -0,0 +1,3 @@
+// crea tu propio archivo llamado "secrets.js" y pon tu api-key
+
+const api_key = "cb38c927d80947fc044c7b82bf521426"

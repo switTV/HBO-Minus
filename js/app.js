e7d17c92f1c4fd6c3b64617383cba12f79cbb93e
swittv <4galarzajuan2004@gmail.com>
2022-06-01 13:46:23 -0300

    migrating to axios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,67 @@
-const url = `https://api.themoviedb.org/3/movie/2?api_key=cb38c927d80947fc044c7b82bf521426`
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
 
-console.log(api_key)
+    params: {
+        "api_key": api_key
+    },
+
+    Headers:{
+        "Content-Type": "application/json;charset=utf-8"
+    }
+})
+
+async function getTrendingMoviesPreview() {
+    const { data } = await api(`trending/movie/day`)
+    const movies = data.results
+    
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList")
+
+        const movieContainer = document.createElement("div")
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement("img")
+        movieImg.classList.add("movie-img")
+
+        movieImg.setAttribute("alt", movie.title)
+        movieImg.setAttribute("src", `https://image.tmdb.org/t/p/w300/${movie.poster_path}`)
+
+        movieContainer.appendChild(movieImg)
+        trendingPreviewMoviesContainer.appendChild(movieContainer)
+
+        console.log(movie.poster_path)
+    });
+
+}
+
+async function getCategoriesPreview() {
+    const { data } = await api(`genre/movie/list`)
+    let counter = 0
+
+    const categories = data.genres
+
+    //* por cada elemento del array de categories vamos a poner una imagen de cada pelicula---------
+
+    categories.forEach(movie => {
+        const PreviewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list")
+
+        const categoryContainer = document.createElement("div")
+        categoryContainer.classList.add('movie-container');
+
+        const categoryTitle = document.createElement("h3")
+        categoryTitle.classList.add("category-title")
+
+        categoryTitle.setAttribute("id", `id${categories[counter].id}`)
+        const categoryTitleText = document.createTextNode(categories[counter].name)
+
+        counter++
+        //* agregando las cosas dentro del HTML-------------
+        categoryTitle.appendChild(categoryTitleText)
+        categoryContainer.appendChild(categoryTitle)
+        PreviewCategoriesContainer.appendChild(categoryContainer)
+    });
+
+}
+
+getTrendingMoviesPreview()
+getCategoriesPreview()
